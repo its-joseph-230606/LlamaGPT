86c8dde97c73894c0aa1408bbca2351f2e1dc512
Joseph Joseph <131424720+its-joseph-230606@users.noreply.github.com>
2023-10-07 10:58:00 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,6 +3,8 @@ import sys
 import time
 client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
 print("Ask a question")
+COLOR_BLUE = '\033[94m'
+COLOR_RED = '\033[91m'
 def autotype(text):
     for char in text:
         sys.stdout.write(char)
@@ -11,8 +13,10 @@ def autotype(text):
         time.sleep(now)
     sys.stdout.write('\n')
 while True:
+  sys.stdout.write(COLOR_RED)
   n = input("You: ")
   question = n
   result = client.predict(question, api_name="/chat_1")
+  sys.stdout.write(COLOR_BLUE)
   autotype(result)
   
